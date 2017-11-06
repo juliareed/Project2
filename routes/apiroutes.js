1c7e8dcc32f332ded8749087f4b30cb447c0d1c3
MikeyDevs <devsmikey@gmail.com>
2017-11-06 11:05:26 -0600

    resolved merged folders

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -1,86 +1,40 @@
-// *********************************************************************************
-// api-routes.js - this file offers a set of routes for displaying and saving data to the db
-// *********************************************************************************
-
-// Dependencies
-// =============================================================
-
-// Requiring our Todo model
+// Requiring our Secret model
 var db = require("../models");
 
 // Routes
 // =============================================================
 module.exports = function(app) {
-  // GET route for getting all of the posts
-  app.get("/api/posts/", function(req, res) {
-    db.Post.findAll({}).then(function(dbPost) {
-      res.json(dbPost);
+  // GET route for getting all of the secrets
+  app.get("/secrets", function(req, res) {
+    db.Secret.findAll({}).then(function(dbSecret) {
+      res.json(dbSecret);
     });
   });
 
-  // Get route for returning posts of a specific category
-  app.get("/api/posts/category/:category", function(req, res) {
-    db.Post
+  // Get route for returning secrets of a specific category
+  app.get("/api/secrets/category/:category", function(req, res) {
+    db.Secret
       .findAll({
         where: {
           category: req.params.category
         }
       })
-      .then(function(dbPost) {
-        res.json(dbPost);
+      .then(function(dbSecret) {
+        res.json(dbSecret);
       });
   });
 
-  // Get rotue for retrieving a single post
-  app.get("/api/posts/:id", function(req, res) {
-    db.Post
-      .findOne({
-        where: {
-          id: req.params.id
-        }
-      })
-      .then(function(dbPost) {
-        res.json(dbPost);
-      });
-  });
 
-  // POST route for saving a new post
-  app.post("/api/posts", function(req, res) {
-    console.log(req.body);
-    db.Post
+  // POST route for saving a new secret
+  app.post("/api/secrets", function(req, res) {
+    db.Secret
       .create({
-        title: req.body.title,
-        body: req.body.body,
-        category: req.body.category
+        secret: req.body.secret,
+        lat: req.body.lat,
+        lng: req.body.lng
       })
-      .then(function(dbPost) {
-        res.json(dbPost);
+      .then(function(dbSecret) {
+        res.json(dbSecret);
       });
   });
-
-  // DELETE route for deleting posts
-  app.delete("/api/posts/:id", function(req, res) {
-    db.Post
-      .destroy({
-        where: {
-          id: req.params.id
-        }
-      })
-      .then(function(dbPost) {
-        res.json(dbPost);
-      });
-  });
-
-  // PUT route for updating posts
-  app.put("/api/posts", function(req, res) {
-    db.Post
-      .update(req.body, {
-        where: {
-          id: req.body.id
-        }
-      })
-      .then(function(dbPost) {
-        res.json(dbPost);
-      });
-  });
-};
+}
