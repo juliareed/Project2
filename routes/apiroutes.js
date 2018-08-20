52d501d14d803ea07ea763d75cd39c206408f1bb
MikeyDevs <devsmikey@gmail.com>
2018-08-19 23:56:41 -0500

    added token security key

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -1,6 +1,12 @@
 // Requiring our Secret model
 var db = require("../models");
 
+//IMPORT NANOID TOKEN GENERATOR AND EXPORT
+var nanoid = require('nanoid');
+
+//CALL NANOID AND PLACE VALUE IN VARIABLE
+var token= nanoid();
+
 // Routes
 // =============================================================
 module.exports = function(app) {
@@ -9,6 +15,11 @@ module.exports = function(app) {
         db.Secret.findAll({}).then(function(dbSecret) {
             res.json(dbSecret);
         });
+    });
+
+    // GET route for getting the token
+    app.get("/index", function(req, res) {
+        res.send(token)
     });
 
     // Get route for returning secrets of a specific category
@@ -20,7 +31,6 @@ module.exports = function(app) {
                 }
             })
             .then(function(dbSecret) {
-              console.log(dbSecret);
               res.send(dbSecret);
               res.redirect("/profile");
             });
@@ -29,15 +39,16 @@ module.exports = function(app) {
 
     // POST route for saving a new secret
     app.post("/api/secrets", function(req, res) {
+        
 
         //FUNCTION TO STRIP HTML TAGS FROM USER INPUT
          function deleteHTML(text){
             return text.replace(/(<([^>]+)>)/ig, "");
          }
 
-         //CONDITIONAL TO ONLY POST IF SECURITY HEADER MATCHES POST REQUEST
-        var securityKey = req.headers["sec103"];
-        if (securityKey && securityKey === "gogoplz56923") {
+         //CONDITIONAL TO ONLY POST IF SECURITY TOKEN MATCHES POST REQUEST
+        var securityKey = req.body.tokenid;
+        if (securityKey && securityKey === token) {
         db.Secret
             .create({
                 secret: deleteHTML(req.body.secret),
